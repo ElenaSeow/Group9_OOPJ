fc102918cd82abec409ce285f60582581f7edea9
Rosh <roshanzkai@gmail.com>
2023-02-22 01:23:44 +0800

    edit admin changed user into abstract class initialized complaint

--- src/main/java/cClasses/Complaint.java
+++ src/main/java/cClasses/Complaint.java
@@ -4,10 +4,28 @@
  */
 package cClasses;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author HP
  */
 public class Complaint {
+    ArrayList<Complaint> complaints = new ArrayList<Complaint>();
+    String complaintId;
+    String desc;
+    String status;
+    Date updateDate;
+    Date complaintDate;
+    
+    public Complaint(String complaintId, String desc, String status, Date complaintDate, Date updateDate){
+        this.complaintId=complaintId;
+        this.desc=desc;
+        this.status=status;
+        this.updateDate=updateDate;
+        this.complaintDate=complaintDate;
+    }
     
 }
+

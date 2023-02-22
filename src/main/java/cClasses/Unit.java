e9cd0173e5abee6f5ca9f5e6b5d0b557b13a2630
Rosh <roshanzkai@gmail.com>
2023-02-22 14:22:45 +0800

    Fk Java

--- src/main/java/cClasses/Unit.java
+++ src/main/java/cClasses/Unit.java
@@ -10,4 +10,75 @@ package cClasses;
  */
 public class Unit {
     private String unitId;
+    private String unitNo;
+    private String block;
+    private int bedroom;
+    private int bathroom;
+    private String status;
+    private String residentId;
+    
+    public Unit(String unitId, String unitNo, String block, int bedroom, int bathroom, String status, String residentId){
+        this.unitId=unitId;
+        this.unitNo=unitNo;
+        this.block=block;
+        this.bedroom=bedroom;
+        this.bathroom=bathroom;
+        this.status=status;
+        this.residentId=residentId;
+    }
+    
+        public Unit(String unitId, String unitNo, String block, String bedroom, String bathroom, String status, String residentId){
+        int Ibedroom = Integer.parseInt(bedroom);
+        int Ibathroom = Integer.parseInt(bathroom);
+            
+        this.unitId=unitId;
+        this.unitNo=unitNo;
+        this.block=block;
+        this.bedroom=Ibedroom;
+        this.bathroom=Ibathroom;
+        this.status=status;
+        this.residentId=residentId;
+    }
+        public String getUnitId(){
+            return unitId;
+        }
+        public String getUnitNo(){
+            return unitNo;
+        }
+        public String getBlock(){
+            return block;
+        }
+        public int getBedroom(){
+            return bedroom;
+        }
+        public int getBathroom(){
+            return bathroom;
+        }
+        public String getStatus(){
+            return status;
+        }
+        public String getResidentId(){
+            return residentId;
+        }
+        
+        public void setUnitNo(String unitNo){
+            this.unitNo=unitNo;
+        }
+        public void setBlock(String block){
+            this.block=block;
+        }
+        public void setBedroom(String bedroom){
+            int bed = Integer.parseInt(bedroom);
+            this.bedroom=bed;
+        }
+        public void setBedroom(int bedroom){
+            this.bedroom=bedroom;
+        }
+        public void setBathroom(String bathroom){
+            int bath = Integer.parseInt(bathroom);
+            this.bathroom=bath;
+        }
+        public void setBathroom(int bathroom){
+            this.bathroom=bathroom;
+        }
 }

b4ffe4dede9d8da64a9dbfdba5042f0e18181e2a
ElenaSeow <elenaseowjy1@gmail.com>
2023-03-02 17:30:54 +0800

    VISITOR DONE

--- src/main/java/cClasses/VisitorPass.java
+++ src/main/java/cClasses/VisitorPass.java
@@ -200,14 +200,12 @@ public class VisitorPass {
     public static void ViewVisitorPass(ArrayList<VisitorPass> visitorpass,JTable table,String id){
             DefaultTableModel model = (DefaultTableModel) table.getModel();
             for(VisitorPass u:visitorpass){
-                String userID=u.getUserId();
-                if(id.equals(userID)){
-//                    DateFormat date_format = new SimpleDateFormat("dd-MM-yyyy");
-//                    String date = date_format.format(u.getDate());
-                
+            String status = u.getStatus();
+                if(status.equals("Approved")) {
                     String[] allDataRow = {u.getVisitorId(),u.getUserId(),u.getName(),u.getContactNo(),u.getPlateNo(),u.getStatus()};
                     model.addRow(allDataRow);
                 }
+     
                 
         }
      }
